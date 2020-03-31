0693ac025e7974d843e09e4bf519c6abb17c81d4
Stefano Facchinetti <34067164+Gorlenah@users.noreply.github.com>
2020-03-31 19:55:39 +0200

    simplified code

--- src/test/java/it/polimi/ingsw/model/parser/DeckReaderTest.java
+++ src/test/java/it/polimi/ingsw/model/parser/DeckReaderTest.java
@@ -6,8 +6,6 @@ import it.polimi.ingsw.model.cards.Type;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,7 +16,7 @@ class DeckReaderTest {
 
         DeckReader deckReader = new DeckReader();
         //load from *.json
-        Deck deck = deckReader.loadDeck(Files.newBufferedReader(Paths.get("src/Divinities.json")));
+        Deck deck = deckReader.loadDeck(new FileReader("src/Divinities.json"));
 
         assertNotNull(deck.getDivinityCard("Apollo"));
         assertSame(deck.getDivinityCard("Apollo").getCardType(), Type.MOVEMENT);
