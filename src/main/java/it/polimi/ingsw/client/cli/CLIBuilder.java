5c05a4d44155ba3ab05eca12c9a4d9a6b6c6a7c0
Dario Crippa <dario2.crippa@mail.polimi.it>
2020-04-20 18:49:55 +0200

    Updated CLI basic objects

--- src/main/java/it/polimi/ingsw/client/cli/CLIBuilder.java
+++ src/main/java/it/polimi/ingsw/client/cli/CLIBuilder.java
@@ -1,4 +1,116 @@
 package it.polimi.ingsw.client.cli;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * CLIBuilder contains everything you need to build the CLI and use it
+ */
 public class CLIBuilder {
+
+    //Data Objects
+    private CLIDataObject[] boardCellsContents; // row ┃   ┃   ┃   ┃   ┃   ┃
+    private List<String> playerMoves;
+    private String currentPhase;
+    private int numberFullTowers;
+
+    //UI Objects
+    protected static final String CLI_INPUT = ">";
+    protected static final String NEW_LINE = "\n";
+    protected static final String BLANK = " ";
+    private static final String BOARD_TITLE = "BOARD";
+    private static final String PLAYERS_TITLE = "PLAYERS";
+    private static final String TOWERS_TITLE = "FULL TOWERS";
+    private static final String MOVES_TITLE = "PLAYER MOVES";
+    private static final String PHASE_TITLE = "CURRENT PHASE";
+
+    //Board Matrix
+    protected static final String L_T_CORNER = "┏";
+    protected static final String R_T_CORNER = "┓";
+    protected static final String L_B_CORNER = "┗";
+    protected static final String R_B_CORNER = "┛";
+    protected static final String R_LAT_SEPARATOR = "┫";
+    protected static final String L_LAT_SEPARATOR = "┣";
+    protected static final String INT_SEPARATOR = "╋";
+    protected static final String U_LAT_SEPARATOR = "┳";
+    protected static final String LO_LAT_SEPARATOR = "┻";
+    protected static final String H_LINE = "━";
+    protected static final String V_LINE = "┃";
+
+    //Players Box
+    private static final String DOUBLE_L_T_CORNER = "╔";
+    private static final String DOUBLE_R_T_CORNER = "╗";
+    private static final String DOUBLE_L_B_CORNER = "╚";
+    private static final String DOUBLE_R_B_CORNER = "╝";
+    private static final String DOUBLE_H_LINE = "═";
+    private static final String DOUBLE_V_LINE = "║";
+
+    //Data Boxes
+    protected static final String DOT_H_LINE = "╍";
+    protected static final String DOT_V_LINE = "╏";
+
+    //ANSI Colors
+    private static final String CODE_BLUE ="33";
+    private static final String CODE_LIGHTBLUE ="75";
+    private static final String CODE_BROWN ="130";
+    private static final String CODE_GRAY ="252";
+    private static final String CODE_WHITE ="255";
+    private static final String CODE_RED = "197";
+    private static final String CODE_GREEN = "41";
+    private static final String ANSI_PRFX ="\u001b[38;5;";
+    protected static final String ANSI_RST = "\u001b[0m";
+
+    protected static final String ANSI_BLUE = ANSI_PRFX+CODE_BLUE+"m";
+    protected static final String ANSI_LIGHTBLUE = ANSI_PRFX+CODE_LIGHTBLUE+"m";
+    protected static final String ANSI_BROWN = ANSI_PRFX+CODE_BROWN+"m";
+    protected static final String ANSI_GRAY = ANSI_PRFX+CODE_GRAY+"m";
+    protected static final String ANSI_WHITE = ANSI_PRFX+CODE_WHITE+"m";
+    protected static final String ANSI_RED = ANSI_PRFX+CODE_RED+"m";
+    protected static final String ANSI_GREEN = ANSI_PRFX+CODE_GREEN+"m";
+
+    //ANSI Cursor Moves
+    protected static final String CURSOR_UP = "\u001b[%sA";
+    protected static final String CURSOR_DWN = "\u001b[%sB";
+    protected static final String CURSOR_LFT = "\u001b[%sD";
+    protected static final String CURSOR_RGT = "\u001b[%sC";
+
+    //ANSI Special Sequences
+    protected static final String CLEAN = "\u001b[0J";
+
+    //Templates
+    private static final String upperEdgeBoard =
+            L_T_CORNER+H_LINE+H_LINE+H_LINE+U_LAT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+U_LAT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+U_LAT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+U_LAT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+R_T_CORNER;
+    private static final String intermediateEdgeBoard =
+            L_LAT_SEPARATOR+H_LINE+H_LINE+H_LINE+INT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+INT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+INT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+INT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+R_LAT_SEPARATOR;
+    private static final String lowerEdgeBoard =
+            L_B_CORNER+H_LINE+H_LINE+H_LINE+LO_LAT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+LO_LAT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+LO_LAT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+LO_LAT_SEPARATOR+
+            H_LINE+H_LINE+H_LINE+R_B_CORNER;
+    private static final String edge_distance = BLANK+BLANK+BLANK;
+    private static final String playerInformations = " %s %s|%s "; // ᳵ SteveJobs|Athena
+    private static final String playerMove = " [%s|%s] "; // [1|2]
+
+    //Sizes
+    private final int refreshable_area_height = 15;
+    private final int editable_board_rows = 5;
+
+    /**
+     * Class Constructor
+     */
+    public CLIBuilder() {
+        this.boardCellsContents = new CLIDataObject[5];
+        this.playerMoves = new ArrayList<>();
+        this.currentPhase = null;
+        this.numberFullTowers = 0;
+    }
 }
--- src/main/java/it/polimi/ingsw/client/cli/CLIDataObject.java
+++ src/main/java/it/polimi/ingsw/client/cli/CLIDataObject.java
@@ -1,4 +1,73 @@
 package it.polimi.ingsw.client.cli;
 
+import it.polimi.ingsw.model.Block;
+import it.polimi.ingsw.model.Color;
+
+import java.util.HashMap;
+
 public class CLIDataObject {
+
+    //ANSI Colors
+    private static final String CODE_BLUE ="33";
+    private static final String CODE_LIGHTBLUE ="75";
+    private static final String CODE_BROWN ="130";
+    private static final String CODE_GRAY ="252";
+    private static final String CODE_WHITE ="255";
+    private static final String CODE_GREEN = "41";
+    private static final String ANSI_PRFX ="\u001b[38;5;";
+    protected static final String ANSI_RST = "\u001b[0m";
+    protected static final String ANSI_BLUE = ANSI_PRFX+CODE_BLUE+"m";
+    protected static final String ANSI_LIGHTBLUE = ANSI_PRFX+CODE_LIGHTBLUE+"m";
+    protected static final String ANSI_BROWN = ANSI_PRFX+CODE_BROWN+"m";
+    protected static final String ANSI_GRAY = ANSI_PRFX+CODE_GRAY+"m";
+    protected static final String ANSI_WHITE = ANSI_PRFX+CODE_WHITE+"m";
+    protected static final String ANSI_GREEN = ANSI_PRFX+CODE_GREEN+"m";
+
+    //Game Objects
+    protected static final String WORKER = "ᳵ";
+    protected static final String DOME = ANSI_LIGHTBLUE+"◉"+ANSI_RST;
+    protected static final String GRASS = ANSI_GREEN+"᭟"+ANSI_RST;
+
+    //Data
+    private static final String cellDataTemplate = "%s %s";
+    private static final String freeCellTemplate = "  "+GRASS;
+    private String[] cellsData; // ┃col1┃col2┃col3┃col4┃col5┃
+    private HashMap<Block,String> towersInformations;
+    private HashMap<Color,String> colorsInformations;
+
+    /**
+     * Class Constructor : generates an empty row
+     */
+    public CLIDataObject() {
+        this.cellsData = new String[5];
+        this.towersInformations = new HashMap<>();
+        this.colorsInformations = new HashMap<>();
+        for(String current : cellsData){
+            current=freeCellTemplate;
+        }
+        towersInformations.put(Block.GROUND,GRASS);
+        towersInformations.put(Block.LEVEL_1,ANSI_WHITE+"1"+ANSI_RST);
+        towersInformations.put(Block.LEVEL_2,ANSI_WHITE+"2"+ANSI_RST);
+        towersInformations.put(Block.LEVEL_3,ANSI_WHITE+"3"+ANSI_RST);
+        towersInformations.put(Block.DOME,DOME);
+        colorsInformations.put(Color.BLUE,ANSI_BLUE+WORKER+ANSI_RST);
+        colorsInformations.put(Color.BROWN,ANSI_BROWN+WORKER+ANSI_RST);
+        colorsInformations.put(Color.GREY,ANSI_GRAY+WORKER+ANSI_RST);
+    }
+
+    /**
+     * Writes data inside the row
+     */
+    public void writeData(){
+
+    }
+    /**
+     * Resets the row to initial state
+     */
+    public void resetData(){
+        for(String current : cellsData){
+            current=freeCellTemplate;
+        }
+
+    }
 }
