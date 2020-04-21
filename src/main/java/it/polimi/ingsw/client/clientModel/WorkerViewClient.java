7c7ef9a95a2b4ff05cf69e9b2c3068539dbdfc36
Stefano Facchinetti <34067164+Gorlenah@users.noreply.github.com>
2020-04-21 20:04:23 +0200

    Add WorkerViewClient structure

---
+++ src/main/java/it/polimi/ingsw/client/clientModel/WorkerViewClient.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.client.clientModel;
+
+/**
+ * Class used in update message of worker view
+ */
+public class WorkerViewClient {
+    private CellInterface[][] workerView;
+    private static WorkerViewClient instance = null;
+
+    /**
+     * Factory method that returns the Battlefield instance (Singleton)
+     * @return Battlefield object
+     */
+    public static WorkerViewClient getWorkerViewInstance(){
+        if(instance == null)
+            instance = new WorkerViewClient();
+        return instance;
+    }
+
+    public void setWorkerView(CellInterface[][] workerView) {
+        this.workerView = workerView;
+    }
+
+    public CellInterface getCell(int x, int y){
+        return workerView[x][y];
+    }
+}
