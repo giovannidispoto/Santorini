a662a79735cecf87cbf6367a5e33e9159303c05f
Stefano Facchinetti <34067164+Gorlenah@users.noreply.github.com>
2020-04-22 19:30:58 +0200

    Fix ClientSocketConnection

--- src/main/java/it/polimi/ingsw/client/network/ClientSocketConnection.java
+++ src/main/java/it/polimi/ingsw/client/network/ClientSocketConnection.java
@@ -25,17 +25,23 @@ public class ClientSocketConnection {
 
     /**
      * Start Client Connection with socket to the server
-     * @throws IOException error in socket creation
+     * @return Boolean, true if connection is established
      */
-    public void startConnection() throws IOException {
+    public Boolean startConnection(){
         System.out.println("Connecting to: " + serverName + " on port " + port);
         //open TCP port
-        clientSocket = new Socket(serverName, port);
+        try {
+            clientSocket = new Socket(serverName, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
 
         System.out.println("Just connected to: " + clientSocket.getRemoteSocketAddress());
         // open input and output streams to read and write
         Thread thread = new Thread(new ServerThread(clientSocket, controller));
         thread.start();
+        return true;
     }
 
     //Getter & Setter
