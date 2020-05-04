666c46c22731841bc06c4703198dab0ac734f646
Stefano Facchinetti <34067164+Gorlenah@users.noreply.github.com>
2020-05-04 21:51:38 +0200

    Add countFullTowers method for BattlefieldClient - ClientSide

--- src/main/java/it/polimi/ingsw/client/clientModel/BattlefieldClient.java
+++ src/main/java/it/polimi/ingsw/client/clientModel/BattlefieldClient.java
@@ -6,6 +6,10 @@ import it.polimi.ingsw.client.network.actions.data.dataInterfaces.CellInterface;
  * Class used to represent battlefield in the client
  */
 public class BattlefieldClient {
+    public static final int N_ROWS = 5;
+    public static final int N_COLUMNS = 5;
+    public static final int MAX_TOWER_HEIGHT = 4;
+
     private CellInterface[][] battlefieldBoard;
     private static BattlefieldClient instance = null;
 
@@ -35,5 +39,24 @@ public class BattlefieldClient {
      */
     public boolean isCellOccupied(int x, int y){
         return (null != battlefieldBoard[x][y].getPlayer());
+    }
+
+    /**
+     * Count FullTowers presents on the battlefield
+     * @return int fullTowers number
+     */
+    public int countFullTowers(){
+        int fullTowersCounter = 0;
+
+        //check full towers presents on battlefieldBoard
+        for(int x = 0; x < N_ROWS; x++) {
+            for (int y = 0; y < N_COLUMNS; y++) {
+                //count full towers
+                if (battlefieldBoard[x][y].getHeight() == MAX_TOWER_HEIGHT){
+                    fullTowersCounter++;
+                }
+            }
+        }
+        return fullTowersCounter;
     }
 }
