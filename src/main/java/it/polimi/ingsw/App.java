b4997dbbf8f258667e45451bf9150ef142e5709e
Giovanni Dispoto <dispoto97@gmail.com>
2020-05-11 21:47:28 +0200

    Add JavaFX to pom.xml

---
+++ src/main/java/it/polimi/ingsw/App.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
