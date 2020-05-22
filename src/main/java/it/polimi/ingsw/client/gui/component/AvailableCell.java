96b56bc4ee90db170b66f794c9adfd208689e2ba
Giovanni Dispoto <dispoto97@gmail.com>
2020-05-23 01:22:51 +0200

    Modify SelectCardView adding waiting. Start writing BattlefieldView with initial data

---
+++ src/main/java/it/polimi/ingsw/client/gui/component/AvailableCell.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.client.gui.component;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class AvailableCell extends ImageView {
+
+    public AvailableCell(){
+        super.setImage(new Image(getClass().getResource("/Images/BoardElements/AvailableCell.png").toString()));
+        super.setFitWidth(80);
+        super.setFitHeight(80);
+    }
+}
