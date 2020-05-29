ff9e8e01fae7e2786834a7ba143027c15848ccc4
Stefano Facchinetti <34067164+Gorlenah@users.noreply.github.com>
2020-05-29 10:15:05 +0200

    Improve EXmessages - ClientSide

--- src/main/java/it/polimi/ingsw/client/controller/ExceptionMessages.java
+++ src/main/java/it/polimi/ingsw/client/controller/ExceptionMessages.java
@@ -2,12 +2,12 @@ package it.polimi.ingsw.client.controller;
 
 public final class ExceptionMessages {
     //Error Messages
-    public static final String genericError = "Unknown error";
-    public static final String IOSocketError = "I/O Socket error";
-    public static final String streamDownSocketError = "Server went offline";
-    public static final String pingSocketError = "Network connection absent, game interrupted";
-    public static final String jsonError = "Json error in parsing net-message";
-    public static final String defaultServerError = "Server error";
+    public static final String genericError = "Unexpected Error";
+    public static final String IOSocketError = "I/O Socket Error";
+    public static final String streamDownSocketError = "Server Went Offline or Closed Connection";
+    public static final String pingSocketError = "Network Connection Absent - Game Interrupted";
+    public static final String jsonError = "Json Error Parsing Net-Message";
+    public static final String defaultServerError = "Server Error";
     //Game Messages
     public static final String loseMessage = "You have lost! 😡";
     public static final String winMessage = "You have won! 👑";
