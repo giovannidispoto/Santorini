ffa3e3ddb6f553e576fa8e00e9cb61e1a4391a4b
Giovanni Dispoto <dispoto97@gmail.com>
2020-07-03 09:44:12 +0200

    Fix error handling on malformed server port

--- src/main/java/it/polimi/ingsw/client/gui/LoginView.java
+++ src/main/java/it/polimi/ingsw/client/gui/LoginView.java
@@ -25,87 +25,94 @@ public class LoginView {
      * @param builder GUIBuilder
      */
     public LoginView(Parent root, GUIBuilder builder) {
-            /* Looking up element on fxml */
-            Button btn = (Button) root.lookup("#connectButton");
-            Label terminalLabel = (Label) root.lookup("#terminalLabel");
-            TextField socketPortField = (TextField) root.lookup("#socketPortField");
-            TextField serverIPField = (TextField) root.lookup("#serverIPField");
-            ExecutorService executorService = Executors.newSingleThreadExecutor();
+        /* Looking up element on fxml */
+        Button btn = (Button) root.lookup("#connectButton");
+        Label terminalLabel = (Label) root.lookup("#terminalLabel");
+        TextField socketPortField = (TextField) root.lookup("#socketPortField");
+        TextField serverIPField = (TextField) root.lookup("#serverIPField");
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-            //Disable button
-            btn.setDisable(true);
+        //Disable button
+        btn.setDisable(true);
 
-            //Setup terminal label
-            terminalLabel.setTextFill(Color.web("#FFFFFF",1));
-            terminalLabel.setText("Setup Server Parameters...");
+        //Setup terminal label
+        terminalLabel.setTextFill(Color.web("#FFFFFF",1));
+        terminalLabel.setText("Setup Server Parameters...");
 
-            AtomicBoolean emptyIP = new AtomicBoolean(true);
-            AtomicBoolean emptyPort = new AtomicBoolean(true);
+        AtomicBoolean emptyIP = new AtomicBoolean(true);
+        AtomicBoolean emptyPort = new AtomicBoolean(true);
 
-            /*
-            * Activate button when user insert IP and port
-            * */
-            socketPortField.textProperty().addListener((observable,oldText, newText)->{
-                emptyPort.set(newText.isEmpty());
+        /*
+         * Activate button when user insert IP and port
+         * */
+        socketPortField.textProperty().addListener((observable,oldText, newText)->{
+            emptyPort.set(newText.isEmpty());
 
-                if(!emptyPort.get() && !emptyIP.get())
-                    btn.setDisable(false);
-                else
-                    btn.setDisable(true);
-            });
-
-            serverIPField.textProperty().addListener((observable, oldText, newText)->{
-                emptyIP.set(newText.isEmpty());
-
-                if(!emptyPort.get() && !emptyIP.get())
-                    btn.setDisable(false);
-                else
-                    btn.setDisable(true);
-            });
-
-
-            /* Adding listener to button*/
-            btn.setOnMouseClicked(e->{
-                /* Disable input */
-                terminalLabel.setTextFill(Color.web("#FFFFFF",1));
-                terminalLabel.setText("Handshaking with the Server...");
+            if(!emptyPort.get() && !emptyIP.get())
+                btn.setDisable(false);
+            else
                 btn.setDisable(true);
-                socketPortField.setDisable(true);
-                serverIPField.setDisable(true);
-                int socketPort = Integer.parseInt(socketPortField.getText().trim());
-                String serverIP = serverIPField.getText().trim();
+        });
 
-                /*Sending request to server*/
-                boolean server = GUIController.getController().getSocketConnection().setServerName(serverIP);
+        serverIPField.textProperty().addListener((observable, oldText, newText)->{
+            emptyIP.set(newText.isEmpty());
 
-                if(!server){
-                    Platform.runLater(()-> terminalLabel.setText("This doesn't seem to be a Server..."));
-                    terminalLabel.setTextFill(Color.web("#FC2A5D",1));
-                    socketPortField.setDisable(false);
-                    serverIPField.setDisable(false);
-                    btn.setDisable(false);
-                }else {
-                    /* Try server connection */
-                    Task<Void> connectTask = new Task<Void>() {
-                        @Override
-                        protected Void call() throws Exception {
-                            GUIController.getController().getSocketConnection().setServerPort(socketPort);
-                            boolean result = GUIController.getController().getSocketConnection().startConnection();
-                            if (result) {//If connection is established correctly, change view
-                                Platform.runLater(()->builder.changeView(Optional.empty()));
-                            } else { //If there is a problem with connection, request another time
-                                Platform.runLater(() -> terminalLabel.setText("Something went wrong...retry!"));
-                                terminalLabel.setTextFill(Color.web("#FC2A5D",1));
-                                socketPortField.setDisable(false);
-                                serverIPField.setDisable(false);
-                                btn.setDisable(false);
-                            }
-                            return null;
+            if(!emptyPort.get() && !emptyIP.get())
+                btn.setDisable(false);
+            else
+                btn.setDisable(true);
+        });
+
+
+        /* Adding listener to button*/
+        btn.setOnMouseClicked(e->{
+            int socketPort = 0;
+            boolean invalidPort = false;
+            /* Disable input */
+            terminalLabel.setTextFill(Color.web("#FFFFFF",1));
+            terminalLabel.setText("Handshaking with the Server...");
+            btn.setDisable(true);
+            socketPortField.setDisable(true);
+            serverIPField.setDisable(true);
+            try {
+                socketPort = Integer.parseInt(socketPortField.getText().trim());
+            }catch(NumberFormatException exception){
+                invalidPort = true;
+            }
+            String serverIP = serverIPField.getText().trim();
+
+            /*Sending request to server*/
+            boolean server = GUIController.getController().getSocketConnection().setServerName(serverIP);
+
+            if(!server || invalidPort){
+                Platform.runLater(()-> terminalLabel.setText("This doesn't seem to be a Server..."));
+                terminalLabel.setTextFill(Color.web("#FC2A5D",1));
+                socketPortField.setDisable(false);
+                serverIPField.setDisable(false);
+                btn.setDisable(false);
+            }else {
+                /* Try server connection */
+                int finalSocketPort = socketPort;
+                Task<Void> connectTask = new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        GUIController.getController().getSocketConnection().setServerPort(finalSocketPort);
+                        boolean result = GUIController.getController().getSocketConnection().startConnection();
+                        if (result) {//If connection is established correctly, change view
+                            Platform.runLater(()->builder.changeView(Optional.empty()));
+                        } else { //If there is a problem with connection, request another time
+                            Platform.runLater(() -> terminalLabel.setText("Something went wrong...retry!"));
+                            terminalLabel.setTextFill(Color.web("#FC2A5D",1));
+                            socketPortField.setDisable(false);
+                            serverIPField.setDisable(false);
+                            btn.setDisable(false);
                         }
-                    };
-                    executorService.submit(connectTask);
-                }
-            });
+                        return null;
+                    }
+                };
+                executorService.submit(connectTask);
+            }
+        });
 
     }
 
